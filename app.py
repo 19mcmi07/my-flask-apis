1ba0d9a0f39073d5932b6313eaf4035ef724f438
Rohan Yadav <19mcmi07@gamil.com>
2020-05-31 14:08:36 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+import joblib
+import traceback
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home_route():
+    return jsonify({"txt": "This is the home page of my flask ML apis."})
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if lr:
+        try:
+            json_ = request.json
+            print(json_)
+            query = pd.get_dummies(pd.DataFrame(json_))
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(lr.predict(query))
+
+            return jsonify({'prediction': str(prediction)})
+
+        except:
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        print ('Train the model first')
+        return ('No model here to use')
+
+if __name__ == '__main__':
+
+    lr = joblib.load("titanic_model.pkl")
+    print("model loaded")
+    model_columns = joblib.load("titanic_model_columns.pkl")
+    print("model columns loaded")
+
+    app.run()
